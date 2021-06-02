036fac2f047e4e66fec589669a9772e21113e66d
enPlace <n.place@protonmail.com>
2021-06-02 14:09:27 -0300

    os module info

---
+++ modules.js
@@ -0,0 +1,8 @@
+const {sayHi, test} = require('./utils.js')
+const {john, pete} = require('./names.js')
+// sayHi("susan")
+// sayHi(john)
+// sayHi(pete)
+// test("nick")
+
+require('./mind-grenade.js')
