672bcb8da962e6f55c96763e56751a5cfa326a85
enPlace <n.place@protonmail.com>
2021-06-07 20:38:01 -0300

    more async stuff

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("what what")
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    console.log('request event')
+    res.end("hello")
+})
+
+server.listen(5000, ()=>{
+    console.log("port open at 5000...")
+}) 
---
+++ setInterval.js
@@ -0,0 +1,4 @@
+console.log("first")
+const interval = setInterval(()=>
+console.log("second"), 1000)
+console.log("third")
